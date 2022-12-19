16f83a635e642d2cb794027e9bcb24c185b6b01f
ashraful2020 <mr.ashraful2020@gmail.com>
2022-12-19 22:39:47 +0600

    design home part

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Navigation from '../Shared/Navigation/Navigation';
+
+const Home = () => {
+    const Todos = [
+        {
+            todo: "Lorem ipsum dolor ."
+        }, {
+            todo: "Lorem ipsum dolor sit amet ."
+        }, {
+            todo: "Lorem ipsum dolor sit amet consectetur ."
+        }, {
+            todo: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. "
+        }, {
+            todo: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident cum animi voluptatem ."
+        }]
+    return (
+        <div className='container'>
+            <p className='fs-1 text-primary text-opacity-50 text-center fw-bold display-1'> Unique Todo Application </p>
+
+            <Navigation />
+            <div className='w-50 mx-auto'>
+                <form className="form-floating">
+                    <input className="form-control is-valid" id="floatingInputInvalid" placeholder="name@example.com hello " />
+                    <label for="floatingInputInvalid">Enter Your Todo </label>
+                </form>
+            </div>
+            <div className='w-50 mx-auto text-start fs-6 text-success'>
+
+
+                {
+                    Todos.map((data, _i) => <div className="form-check" key={_i}>
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
+                        <label className="form-check-label" for="flexCheckIndeterminate">
+                            {data.todo}
+                        </label>
+                    </div>)
+                }
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
