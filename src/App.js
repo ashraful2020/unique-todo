a69a513a541cbeee50830c3942640575044e6772
ashraful2020 <mr.ashraful2020@gmail.com>
2022-12-19 22:38:37 +0600

    updated structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Nav from './nav';
+import Home from './pages/Home/Home';
 
 function App() {
 
@@ -19,34 +20,15 @@ function App() {
     }]
   return (
     <>
-      <div className="App container">
-        <p className='fs-1 text-primary text-opacity-50 text-center fw-bold display-1'> Unique Todo Application </p>
-        <Nav />
-        <div className='w-50 mx-auto'>
-          <form className="form-floating">
-            <input className="form-control is-valid" id="floatingInputInvalid" placeholder="name@example.com hello " />
-            <label for="floatingInputInvalid">Enter Your Todo </label>
-          </form>
-        </div>
-        <div className='w-50 mx-auto text-start fs-6 text-success'>
+      <div className="App">
 
-
-          {
-            Todos.map((data, _i) => <div className="form-check" key={_i}>
-              <input className="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
-              <label className="form-check-label" for="flexCheckIndeterminate">
-                {data.todo}
-              </label>
-            </div>)
-          }
-
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<h1>Home app</h1>} />
+          <Route path='/about' element={<h1>about app</h1>} />
+          <Route path='/contact' element={<h1>contact app</h1>} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path='/home' element={<h1>Home app</h1>}/>
-        <Route path='/about' element={<h1>about app</h1>}/>
-        <Route path='/contact' element={<h1>contact app</h1>}/>
-      </Routes>
     </>
   );
 }
